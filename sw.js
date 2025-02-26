5bcc954dbb94fad44ca16466ccbd4790bb586e3b
Bosonian <bosdeepak@gmail.com>
2025-02-26 14:17:46 +0100

    Create sw.js
    created sw.js for cache

---
+++ sw.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = "gfap-cache-v2"; // Increment version on update
+const urlsToCache = [
+    "/",
+    "/index.html",
+    "/manifest.json",
+    "/sw.js",
+    "/icon.png"
+];
+
+// Install and cache files
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(urlsToCache))
+    );
+});
+
+// Activate - Remove old caches when updating
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
+
+// Fetch - Serve from cache if available, otherwise fetch from network
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
