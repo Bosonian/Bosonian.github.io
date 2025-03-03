a89bb1c2625610764fd88794f704e6e0155eaeb3
Bosonian <bosdeepak@gmail.com>
2025-03-03 17:09:43 +0100

    Update sw.js
    hidden formula and access codes added

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "gfap-cache-v14"; // Increment version
+const CACHE_NAME = "gfap-cache-v15"; // Increment version
 const OFFLINE_URL = '/offline.html';
 const urlsToCache = [
   '/',
