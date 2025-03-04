4ecaef8ca867122baae6f9b195f22226cb49213b
Bosonian <bosdeepak@gmail.com>
2025-03-04 20:37:12 +0100

    Update sw.js
    try

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "gfap-cache-v33"; // Increment version
+const CACHE_NAME = "gfap-cache-v34"; // Increment version
 const OFFLINE_URL = '/offline.html';
 const urlsToCache = [
   '/',
