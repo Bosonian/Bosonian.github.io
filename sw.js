cab7f79e6e5e634fece6a8e46fcfac405514f06b
Bosonian <bosdeepak@gmail.com>
2025-03-11 09:55:21 +0100

    Update sw.js
    GPT debug with 2 modules

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "gfap-cache-v65"; // Increment version
+const CACHE_NAME = "gfap-cache-v66"; // Increment version
 const OFFLINE_URL = '/offline.html';
 const urlsToCache = [
   '/',
